77361961ab4740afc3b31999840d6da75b758b0f
NoDebt GitHub Action <action@github.com>
2024-05-14 19:39:32 +0000

    NoDebt Automate file modifications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main(*args, **kwargs):
-    # TODO: Implement the main function
+    # TODO: Implement the main function https://varsity.atlassian.net/browse/HCKDY-930
     
-    # FIXME: this is not working :c
+    # FIXME: this is not working :c https://varsity.atlassian.net/browse/HCKDY-931
     this_is_a_sum = 5 * 5 
 
     return 0
