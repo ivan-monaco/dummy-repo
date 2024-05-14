882ff656b71a8c84370d600a0cf0386f1109f0a1
Ivan Monaco <ivan.monaco@varsitytutors.com>
2024-05-14 12:07:21 -0300

    Added dummy

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 def main(*args, **kwargs):
     # TODO: Implement the main function https://varsity.atlassian.net/browse/HCKDY-928
-    pass
+    
+    this_is_a_sum = 5 * 5 # FIXME: this is not working :c
+
+    return 0
