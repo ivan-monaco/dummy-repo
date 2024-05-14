62b4e540f437318ba4d56d4523e67a180b99e2b3
Ivan Monaco <ivan.monaco@varsitytutors.com>
2024-05-14 12:12:24 -0300

    Change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main(*args, **kwargs):
     # TODO: Implement the main function https://varsity.atlassian.net/browse/HCKDY-928
     
-    this_is_a_sum = 5 * 5 # FIXME: this is not working :c
+    # FIXME: this is not working :c
+    this_is_a_sum = 5 * 5 
 
     return 0
